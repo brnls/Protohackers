012b59dc176253e9c519b327b50f19c34e807cae
brnls <brnls1234@gmail.com>
2022-11-28 15:48:34 -0800

    problem 0

---
+++ Problem0.cs
@@ -0,0 +1,44 @@
+﻿namespace ProtoHackers;
+using System.IO.Pipelines;
+using System.Net.Sockets;
+using System.Net;
+
+public class Problem0_EchoServer
+{
+    public static async Task Init(int port)
+    {
+        var listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        listenSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+
+        listenSocket.Listen();
+
+        while (true)
+        {
+            var socket = await listenSocket.AcceptAsync();
+            _ = ProcessLinesAsync(socket);
+        }
+    }
+
+    static async Task ProcessLinesAsync(Socket socket)
+    {
+        using var stream = new NetworkStream(socket, true);
+        var reader = PipeReader.Create(stream);
+
+        while (true)
+        {
+            ReadResult result = await reader.ReadAsync();
+
+            foreach (var mem in result.Buffer)
+            {
+                await stream.WriteAsync(mem);
+            }
+
+            reader.AdvanceTo(result.Buffer.End);
+
+            if (result.IsCompleted)
+            {
+                break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 C# solutions for [Protohackers](https://protohackers.com)
-
-- [Problem 1 - Echo Server](Problem0_EchoServer.cs)
